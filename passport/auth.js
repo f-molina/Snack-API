2511d608ebe1f7cf7bf50e8f2c26ceeaa5e77901
f-molina <00300917@uca.edu.sv>
2019-06-20 23:04:35 -0600

    created the passport auth middleware

---
+++ passport/auth.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const userModel = require('../models/user');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await userModel.findById(id);
+  done(null, user);
+});
+
+passport.use('local-register', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+},  
+ async (req, email, password, done) => {
+  const user = await userModel.findOne({'email': email})
+  console.log(user)
+  if(user) {
+    return done(null, false, req.flash('signupMessage', 'The Email is already Taken.'));
+  } else {
+    const newUser = new userModel();
+    newUser.email = email;
+    newUser.password = newUser.encryptPassword(password);
+  console.log(newUser)
+    await newUser.save();
+    done(null, newUser);
+  }
+}));
+
+passport.use('local-login', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  const user = await userModel.findOne({email: email});
+  if(!user) {
+    return done(null, false, req.flash('signinMessage', 'No User Found'));
+  }
+  if(!user.comparePassword(password)) {
+    return done(null, false, req.flash('signinMessage', 'Incorrect Password'));
+  }
+  return done(null, user);
+}));
