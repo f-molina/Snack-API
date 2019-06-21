21ff5d6ffda6dcad81ab174b580a5b07a98ee93c
f-molina <00300917@uca.edu.sv>
2019-06-20 23:01:14 -0600

    modified user schema and added new dependencies

--- models/user.js
+++ models/user.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    name: {type: String, required: true, unique: true},
     email: {type: String, required: true},
     password: {type: String, required: true}
 });
+
+userSchema.methods.encryptPassword = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+  
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
