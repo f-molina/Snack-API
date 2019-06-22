7e88fbbc34dc9e55feafb1e8fa1daab511d541c3
f-molina <00300917@uca.edu.sv>
2019-06-21 21:52:11 -0600

    added getUsers and logout

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,16 +2,38 @@ const User = require('../models/user');
 const userController = {};
 const passport = require('passport');
 
+//register user
 userController.register = passport.authenticate('local-register', {
-    successRedirect: '/login',
-    failureRedirect: '/register',
+    successRedirect: '/api/v1/users/profile',
+    failureRedirect: '/api/v1/users/register',
     failureFlash: true
-});
+}), (req, res, next) => {
+    res.status(200).json({"message": "Registered"});
+};
 
+//login user
 userController.login = passport.authenticate('local-login', {
-    successRedirect: '/api/v1/api-docs',
-    failureRedirect: '/login',
+    successRedirect: '/api/v1/users/profile',
+    failureRedirect: '/api/v1/users/login',
     failureFlash: true
-});
+}), (req, res, next) => {
+    res.status(200).json({"message": "logged in"});
+};
+
+//logout
+userController.logout = (req, res, next) => {
+    req.logout();
+    res.status(200).json({"message": "logged out"});
+};
+
+//list of users
+userController.getUsers = (req, res, next) => {
+    let listSort = {name: 1};
+    User.find({}, (err, users) => {
+        if(err)
+        return res.status(400).send({"err": err});
+        return res.status(200).send({users});
+    }).sort(listSort);
+};
 
 module.exports = userController;
