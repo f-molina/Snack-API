47ece23c10d451ce304588197cb061fb5609ee6e
f-molina <00300917@uca.edu.sv>
2019-06-21 22:40:22 -0600

    added yaml for swagger ui to user routes

--- routes/users.js
+++ routes/users.js
@@ -2,37 +2,153 @@ const router = require('express').Router();
 
 const userController = require('../controllers/UserController');
 
+/**
+* @swagger
+* definitions:
+*   user:
+*     properties:
+*       email:
+*         type: string
+*       password:
+*         type: string
+*       required:
+*          - email
+*          - password
+*/
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/register', (req, res, next) => {
-  res.render('register');
+/**
+* @swagger
+* /users:
+*   get:
+*     tags:
+*       - users
+*     summary: List of all users
+*     description: Returns all users
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description: An array of users
+*         schema:
+*           $ref: '#/definitions/user'
+*/
+router.get('/users', userController.getUsers);
+
+router.get('/users/register', (req, res, next) => {
+  //res.render('register');
 });
 
-router.post('/register', userController.register);
+/**
+* @swagger
+* /users/register:
+*   post:
+*     tags:
+*       - users
+*     summary: Register an user
+*     description: Register user with both email and password
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: body 
+*         in: body
+*         schema:
+*           $ref: '#/definitions/user'
+*           properties:
+*              email:
+*                type: string
+*              password:
+*                type: string
+*         required:
+*          - email
+*          - password
+*     responses:
+*       200:
+*         description: Registered!
+*/
+router.post('/users/register', userController.register);
 
-router.get('/login', (req, res, next) => {
-  res.render('login');
+router.get('/users/login', (req, res, next) => {
+  //res.render('login');
 });
 
-router.post('/login', userController.login);
+/**
+* @swagger
+* /users/login:
+*   post:
+*     tags:
+*       - users
+*     summary: Login an user
+*     description: Login user with both email and password
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: body 
+*         in: body
+*         schema:
+*           $ref: '#/definitions/user'
+*           properties:
+*              email:
+*                type: string
+*              password:
+*                type: string
+*         required:
+*          - email
+*          - password
+*     responses:
+*       200:
+*         description: Logged in!
+*/
+router.post('/users/login', userController.login);
 
-router.get('/api/v1/api-docs', isValidated, (req, res, next) => {
-  res.render('/api-docs');
+/**
+* @swagger
+* /users/profile:
+*   get:
+*     tags:
+*       - users
+*     summary: Show user info
+*     description: Returns user info
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description: users info
+*         schema:
+*           $ref: '#/definitions/user'
+*/
+router.get('/users/profile', isLoggedIn, (req, res, next) => {
+  res.status(200).json(req.user);
 });
 
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
-});
+/**
+* @swagger
+* /users/logout:
+*   get:
+*     tags:
+*       - users
+*     summary: Logout
+*     description: Logout user
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description: Logged out!
+*         schema:
+*           $ref: '#/definitions/user'
+*/
+router.get('/users/logout', userController.logout);
 
 
-function isValidated(req, res, next) {
-  if(req.isValidated()) {
-    return next();
-  }
-  res.redirect('/')
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.status(400).json({
+      'message': 'access denied'
+  });
 }
 
 module.exports = router;
