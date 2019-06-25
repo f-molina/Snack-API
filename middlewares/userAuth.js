7d9088dc63eef0cbc26cb201039e66a3f2d87b92
f-molina <00300917@uca.edu.sv>
2019-06-24 19:07:38 -0600

    fixed swagger host path and added error message to auth middleware

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -4,7 +4,9 @@ userAuth.admin = function isAdmin(req, res, next) {
     if(req.isAuthenticated() && req.user.role == 'ADMIN'){
         next()
     } else {
-        res.status(401).json({"message": "not admin"});
+        res.status(401).json({
+            "error": "Unauthorized access",
+            "message": "Not admin"});
     }
 }
 
@@ -12,7 +14,8 @@ userAuth.normalUser = function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
     res.status(401).json({
-        'message': 'access denied'
+        'error': 'Unauthorized access',
+        'message': 'You must login'
     });
 }
 
