af4c0e2774bf12c719ef140aac942d2d2f794d36
f-molina <00300917@uca.edu.sv>
2019-06-24 19:16:07 -0600

    changed render path to main

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('../views/layouts/main');
 });
 
 module.exports = router;
